cc14e46d329771323079c3240e9c29238da06b97
MaximPekshev <info@annasoft.ru>
2023-12-07 07:46:15 +0300

    publish into host

--- server/settings.py
+++ server/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
-                 'sidrograd.annasoft.site',
-                 'www.sidrograd.annasoft.site',]
+                 'avtobm21.annasoft.site',
+                 'www.avtobm21.annasoft.site',]
 
 
 # Application definition
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-TESTING = False
+TESTING = True
 
 if TESTING:
     DATABASES = {
@@ -109,6 +109,11 @@ else:
             'USER': config.DB_USER,
             # 'PASSWORD': os.environ.get("DB_PASSWORD", ""),
             'PASSWORD': config.DB_PASSWORD,
+            "OPTION": {
+                        'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+                                    'charset': 'utf8mb4',
+                                                "autocommit": True,
+                                                        }
         }
     }
 
